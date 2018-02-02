1802682fc5429b0fb01b4fecd2efe88d7e75963b
Govinda Kamath <govinda.kamath@gmail.com>
2018-02-01 21:07:01 -0800

    added code to run

---
+++ python/run.py
@@ -0,0 +1,28 @@
+import os
+import itertools
+import multiprocessing as mp
+import subprocess
+import sys
+import numpy as np
+
+def callKNN(command):
+    # subprocess.call(command.split())
+    print command
+
+numThreads = int(sys.argv[1])
+maxPoints = float(sys.argv[2])
+
+chunkSize = np.ceil(maxPoints/float(numThreads))
+
+cmdVec = []
+for i in np.arange(int(maxPoints), step=int(chunkSize)):
+    start = i
+    end = i + int(chunkSize)
+
+    cmd = './build/test_code nominal.ini '+ str(start) + " " + str(end)
+    cmdVec.append(cmd)
+
+pool=mp.Pool(processes=numThreads)
+pool.map(callKNN,cmdVec)
+
+
