88bc3fa901574c6bd4b0a277053ff83c6aa4f024
Govinda Kamath <govinda.kamath@gmail.com>
2018-02-01 16:50:52 -0800

    wrote utils lib

---
+++ cpp/include/utils.h
@@ -0,0 +1,28 @@
+//
+// Created by Govinda Kamath on 2/1/18.
+//
+
+#include <vector>
+#include <queue>
+#include <string>
+
+#ifndef COMBINATORIAL_MAB_UTILS_H
+#define COMBINATORIAL_MAB_UTILS_H
+
+#endif //COMBINATORIAL_MAB_UTILS_H
+
+namespace utils{
+    //Reads the protected container object of a priority queue to give access to its elements
+    //A beautiful hack picked up from https://stackoverflow.com/a/12886393/3377314
+    template <class T, class S, class C>
+    S& Container(std::priority_queue<T, S, C>& q) {
+        struct HackedQueue : private std::priority_queue<T, S, C> {
+            static S& Container(std::priority_queue<T, S, C>& q) {
+                return q.*&HackedQueue::c;
+            }
+        };
+        return HackedQueue::Container(q);
+    }
+
+    void readImageAsVector (std::string filePath, std::vector<float> &imageVec);
+}
