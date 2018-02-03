0a04dfa51060e68864d795f06ef06cc691cfb9b8
Govinda Kamath <govinda.kamath@gmail.com>
2018-02-03 10:49:49 -0800

    Enable separate builds for knn and medoid
    Signed-off-by: Govinda Kamath <govinda.kamath@gmail.com>

--- utils/build.sh
+++ utils/build.sh
@@ -4,7 +4,16 @@ pwd=$PWD
 
 mkdir -p build
 cd $pwd/build
-cmake .. -DCMAKE_C_COMPILER=gcc-4.9 -DCMAKE_CXX_COMPILER=g++-4.9
+
+if [ "$1" == "knn" ]
+then
+    cmake .. -DCMAKE_C_COMPILER=gcc-4.9 -DCMAKE_CXX_COMPILER=g++-4.9 -DMedoid=OFF
+elif [ "$1" == "medoid" ]
+then
+    cmake .. -DCMAKE_C_COMPILER=gcc-4.9 -DCMAKE_CXX_COMPILER=g++-4.9 -DKNN=OFF
+else
+    cmake .. -DCMAKE_C_COMPILER=gcc-4.9 -DCMAKE_CXX_COMPILER=g++-4.9
+fi
 make 
 #make install
 
