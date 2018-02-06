29e568f7a19e27615a5bb6eb95ad130e24f696b7
Govinda Kamath <govinda.kamath@gmail.com>
2018-02-06 10:49:48 -0800

    build
    Signed-off-by: Govinda Kamath <govinda.kamath@gmail.com>

--- utils/build.sh
+++ utils/build.sh
@@ -7,10 +7,13 @@ cd $pwd/build
 
 if [ "$1" == "knn" ]
 then
-    cmake .. -DCMAKE_C_COMPILER=gcc-4.9 -DCMAKE_CXX_COMPILER=g++-4.9 -DMedoid=OFF
+    cmake .. -DCMAKE_C_COMPILER=gcc-4.9 -DCMAKE_CXX_COMPILER=g++-4.9 -DKNN=ON
 elif [ "$1" == "medoid" ]
 then
-    cmake .. -DCMAKE_C_COMPILER=gcc-4.9 -DCMAKE_CXX_COMPILER=g++-4.9 -DKNN=OFF
+    cmake .. -DCMAKE_C_COMPILER=gcc-4.9 -DCMAKE_CXX_COMPILER=g++-4.9 -DMedoid=ON
+elif [ "$1" == "kmeans" ]
+then
+    cmake .. -DCMAKE_C_COMPILER=gcc-4.9 -DCMAKE_CXX_COMPILER=g++-4.9 -DKmeans=ON
 else
     cmake .. -DCMAKE_C_COMPILER=gcc-4.9 -DCMAKE_CXX_COMPILER=g++-4.9
 fi
