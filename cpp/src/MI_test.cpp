3e530d8915b6b986357c4748122d174c1ad44c58
vivekkumarbagaria <vivekee047@gmail.com>
2018-05-08 17:58:59 -0700

    Gaussian MI and entropy working

--- cpp/src/MI_test.cpp
+++ cpp/src/MI_test.cpp
@@ -54,44 +54,44 @@ int main(int argc, char *argv[]){
     }
 
     //Arm1
-    std::vector<SquaredEuclideanPoint> arm1vec, arm2vec;
-    for (int i=0; i<n; ++i) {
-
-        float number1 = (float) distribution1(generator);
-        float number2 = (float) distribution1(generator);
-        std::vector<float> tmp1 = {number1, number1+number2};
-        arm1vec.push_back(SquaredEuclideanPoint(tmp1));
-
-
-        number1 = (float) distribution2(generator);
-        number2 = (float) distribution2(generator);
-        std::vector<float> tmp2 = {number1, 2*number1+number2};
-        arm2vec.push_back(SquaredEuclideanPoint(tmp2));
-    }
-    std::ofstream file2;
-    file2.open("/Users/vivekkumarbagaria/Code/combinatorial_MAB/gaussians", std::ofstream::out | std::ofstream::trunc);
-
-    for (long i = 0; i < n; i++) {
-        file2 << arm1vec[i].point[0] << "," << arm1vec[i].point[1] <<std::endl;
-//        return arr;
-    }
-
-    Arm2DMutualInformation<SquaredEuclideanPoint> arm1(1, arm1vec, indices);
-    Arm2DMutualInformation<SquaredEuclideanPoint> arm2(1, arm2vec, indices);
-
-    std::vector<Arm2DMutualInformation<SquaredEuclideanPoint>> allArmsVec;
-    allArmsVec.push_back(arm1);
-    allArmsVec.push_back(arm2);
-
-    for(int i=0;i < 5; i++){
-        arm1.pullArm(0, 0, 1, true, (int) n/4, -1);
-        arm2.pullArm(0, 0, 1, true, (int) n/4, -1);
-        std::cout << "Arm 1 " << arm1.lowerConfidenceBound << " "
-                  << arm1.estimateOfMean << " " << arm1.upperConfidenceBound << " " << "0.346" << std::endl;
-        std::cout << "Arm 2 " << arm2.lowerConfidenceBound << " "
-                  << arm2.estimateOfMean << " " << arm2.upperConfidenceBound << " " << 0.80 << std::endl;
-        std::cout << arm2.estimateOfMean - arm1.estimateOfMean << " 0.454\n" <<std::endl;
-
-    }
+//    std::vector<SquaredEuclideanPoint> arm1vec, arm2vec;
+//    for (int i=0; i<n; ++i) {
+//
+//        float number1 = (float) distribution1(generator);
+//        float number2 = (float) distribution1(generator);
+//        std::vector<float> tmp1 = {number1, number1+number2};
+//        arm1vec.push_back(SquaredEuclideanPoint(tmp1));
+//
+//
+//        number1 = (float) distribution2(generator);
+//        number2 = (float) distribution2(generator);
+//        std::vector<float> tmp2 = {number1, 2*number1+number2};
+//        arm2vec.push_back(SquaredEuclideanPoint(tmp2));
+//    }
+//    std::ofstream file2;
+//    file2.open("/Users/vivekkumarbagaria/Code/combinatorial_MAB/gaussians", std::ofstream::out | std::ofstream::trunc);
+//
+//    for (long i = 0; i < n; i++) {
+//        file2 << arm1vec[i].point[0] << "," << arm1vec[i].point[1] <<std::endl;
+////        return arr;
+//    }
+//
+//    Arm2DMutualInformation<SquaredEuclideanPoint> arm1(1, arm1vec, indices);
+//    Arm2DMutualInformation<SquaredEuclideanPoint> arm2(1, arm2vec, indices);
+//
+//    std::vector<Arm2DMutualInformation<SquaredEuclideanPoint>> allArmsVec;
+//    allArmsVec.push_back(arm1);
+//    allArmsVec.push_back(arm2);
+//
+//    for(int i=0;i < 5; i++){
+//        arm1.pullArm(0, 0, 1, true, (int) n/4, -1);
+//        arm2.pullArm(0, 0, 1, true, (int) n/4, -1);
+//        std::cout << "Arm 1 " << arm1.lowerConfidenceBound << " "
+//                  << arm1.estimateOfMean << " " << arm1.upperConfidenceBound << " " << "0.346" << std::endl;
+//        std::cout << "Arm 2 " << arm2.lowerConfidenceBound << " "
+//                  << arm2.estimateOfMean << " " << arm2.upperConfidenceBound << " " << 0.80 << std::endl;
+//        std::cout << arm2.estimateOfMean - arm1.estimateOfMean << " 0.454\n" <<std::endl;
+//
+//    }
 
 }
