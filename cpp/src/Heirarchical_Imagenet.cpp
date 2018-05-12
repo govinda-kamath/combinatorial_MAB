782b8b3f4be5baff83d9c1a4839f9e134dca9da4
vivekkumarbagaria <vivekee047@gmail.com>
2018-05-12 16:53:28 -0700

    .

--- cpp/src/Heirarchical_Imagenet.cpp
+++ cpp/src/Heirarchical_Imagenet.cpp
@@ -20,7 +20,7 @@
 
 int main(int argc, char *argv[]) {
     std::string nameConfig = argv[1];
-    std::srand(atoi(argv[2]);
+    std::srand(atoi(argv[2]));
     //    long n(atol(argv[3]));
 
 //    std::string nameConfig = "/Users/vivekkumarbagaria/Code/combinatorial_MAB/nominal.ini";
